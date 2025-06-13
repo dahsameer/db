aac17cbf637b76715e2358ed5451157d6bd9a395
Sameer Dahal <dahsameer@gmail.com>
2025-06-13 15:27:44 +0545

    added methods for meta commands and queries

--- src/main.zig
+++ src/main.zig
@@ -33,6 +33,11 @@ fn readInput(input_buffer: *InputBuffer) !void {
     input_buffer.buffer.clearRetainingCapacity();
 
     try stdin.readUntilDelimiterArrayList(&input_buffer.buffer, '\n', 1024);
+    if (@import("builtin").os.tag == .windows) {
+        if (input_buffer.buffer.items.len > 0 and input_buffer.buffer.items[input_buffer.buffer.items.len - 1] == '\r') {
+            _ = input_buffer.buffer.pop();
+        }
+    }
 }
 
 pub fn main() !void {
@@ -44,8 +49,8 @@ pub fn main() !void {
     defer input_buffer.deinit();
 
     while (true) {
+        var stmt = Statement{ .stmt_type = .Select };
         printPrompt();
-
         readInput(&input_buffer) catch |err| switch (err) {
             error.EndOfStream => {
                 print("Goodbye!\n", .{});
@@ -59,11 +64,97 @@ pub fn main() !void {
 
         const input = input_buffer.getInput();
 
-        if (std.mem.eql(u8, input, ".exit")) {
-            print("Goodbye!\n", .{});
-            return;
-        } else {
-            print("Unrecognized command '{s}'.\n", .{input});
+        if (input[0] == '.') {
+            const meta_result = do_meta_command(input);
+            switch (meta_result) {
+                .Success => {
+                    continue;
+                },
+                .Exit => {
+                    print("Exiting the program.\n", .{});
+                    return;
+                },
+                else => {
+                    print("Unrecognized command '{s}'.\n", .{input});
+                    continue;
+                },
+            }
         }
+        const prepare_result = prepare_statement(input, &stmt);
+        switch (prepare_result) {
+            .Success => {
+                print("Statement prepared successfully: {s}\n", .{input});
+            },
+            else => {
+                print("Error preparing statement: {s}\n", .{input});
+                continue;
+            },
+        }
+        execute_statement(&stmt);
     }
 }
+
+pub fn do_meta_command(input: []const u8) MetaCommandResult {
+    if (std.mem.eql(u8, input, ".exit")) {
+        return .Exit;
+    } else if (std.mem.eql(u8, input, ".help")) {
+        print("Available commands:\n", .{});
+        print(".exit - Exit the program\n", .{});
+        print(".help - Show this help message\n", .{});
+        return .Success;
+    } else {
+        return .UnrecognizedCommand;
+    }
+}
+
+pub fn prepare_statement(input: []const u8, stmt: *Statement) PrepareResult {
+    if (std.mem.startsWith(u8, input, "select")) {
+        stmt.stmt_type = .Select;
+        return .Success;
+    } else if (std.mem.startsWith(u8, input, "insert")) {
+        stmt.stmt_type = .Insert;
+        return .Success;
+    } else if (std.mem.startsWith(u8, input, "update")) {
+        stmt.stmt_type = .Update;
+        return .Success;
+    } else if (std.mem.startsWith(u8, input, "delete")) {
+        stmt.stmt_type = .Delete;
+        return .Success;
+    } else {
+        return .UnrecognizedStatement;
+    }
+}
+
+pub fn execute_statement(stmt: *Statement) void {
+    switch (stmt.stmt_type) {
+        .Select => {
+            print("Executing SELECT statement.\n", .{});
+        },
+        .Insert => {
+            print("Executing INSERT statement.\n", .{});
+        },
+        .Update => {
+            print("Executing UPDATE statement.\n", .{});
+        },
+        .Delete => {
+            print("Executing DELETE statement.\n", .{});
+        },
+    }
+}
+
+pub const MetaCommandResult = enum { Exit, Success, UnrecognizedCommand };
+
+pub const PrepareResult = enum {
+    Success,
+    SyntaxError,
+    UnrecognizedStatement,
+};
+
+pub const StatementType = enum {
+    Select,
+    Insert,
+    Update,
+    Delete,
+};
+
+pub const Statement = struct { stmt_type: StatementType };
